a49635976d9afbcb81152d5722ba8b31e98f8014
joaomamede <joaovpmamede@gmail.com>
2013-09-10 11:39:09 +0100

    changed grunt config a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,11 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    properties: {
+      build_dir:     'build',
+      build_css_dir: '<%= properties.build_dir %>/css',
+      build_js_dir:  '<%= properties.build_dir %>/js'
+    },
     // Metadata.
     meta: {
       version: '0.1.0'
@@ -12,55 +17,20 @@ module.exports = function(grunt) {
       '* http://PROJECT_WEBSITE/\n' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
       'YOUR_NAME; Licensed MIT */\n',
-    // Task configuration.
-    concat: {
+    sass: {
       options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+        style: 'compressed'
       },
       dist: {
-        src: ['public/javascripts/libs/reqwest.js', 'public/javascripts/libs/spin.js', 'public/javascripts/libs/ladda.js', 'public/javascripts/main.js'],
-        dest: 'public/javascripts/all.js'
+        files: {
+          '<%= properties.build_css_dir %>/style.css': 'css/style.scss'
+        }
       }
     },
     uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: 'public/javascripts/all.js',
-        dest: 'public/javascripts/all.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      }
-    },
-    cssmin: {
-      minify: {
-        expand: true,
-        cwd: 'public/stylesheets/',
-        src: ['main.css', 'admin.css', '!*.min.css'],
-        dest: 'public/stylesheets/',
-        ext: '.min.css'
+      build: {
+        src: ['js/libs/*.js', 'js/plugins/*.js', 'js/*.js'],
+        dest: '<%= properties.build_js_dir %>/main.min.js'
       }
     },
     watch: {
@@ -68,28 +38,20 @@ module.exports = function(grunt) {
         livereload: true,
         spawn: false
       },
-      html: {
-        files: 'views/*.erb'
-      },
-      css: {
-        files: '**/*.css',
-        tasks: ['concat', 'cssmin']
-      },
-      js: {
-        files: ['public/javascripts/main.js'],
-        tasks: ['concat', 'uglify']
-      }
+      files: ['**/*'],
+      tasks: ['sass', 'uglify']
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  // Register tasks.
+  grunt.registerTask('build', ['sass', 'uglify']);
+
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
+  grunt.registerTask('default', ['build']);
 
 };
